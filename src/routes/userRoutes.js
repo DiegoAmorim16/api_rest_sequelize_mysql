f921b834ad8e5127b60fc1a2ba5a896fe0d8811e
DiegoAmorim16 <122192252+DiegoAmorim16@users.noreply.github.com>
2023-03-03 20:05:00 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
 
-//import loginRequired from '../middlewares/loginRequired';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
 // Não deveria existir
-router.get('/', userController.index); // Lista usuários
-router.get('/:id', userController.show); // Lista usuário
+router.get('/', loginRequired, userController.index); // Lista usuários
+router.get('/:id', loginRequired, userController.show); // Lista usuário
 router.post('/', userController.create);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/:id', loginRequired, userController.update);
+router.delete('/:id', loginRequired, userController.delete);
 
 export default router;
